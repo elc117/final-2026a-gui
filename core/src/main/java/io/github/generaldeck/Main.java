d270afe8317916810aa519fbc8b74711e0595dc5
Guilherme Martini <guilherme.dcmartini@gmail.com>
2026-06-18 21:56:02 -0300

    feature/ ponteiro personalizado mt bonitinho

--- core/src/main/java/io/github/generaldeck/Main.java
+++ core/src/main/java/io/github/generaldeck/Main.java
@@ -4,28 +4,29 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Cursor;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
-    // assetManager gerencia os recursos. centraliza o carregamento das imagens, sprites, sons
     private AssetManager assetManager;
-    // o Skin funciona como um stylesheet
-    // vamos criar arquivos .atlas e .json para controlar a direção de arte do jogo
-    // por enquanto esses arquivos são "defaults" que eu peguei do próprio libgdx, não representam o resultado final
-    // depois fica fácil de trocar o estilo do jogo por que é só trocar esses arquivos
     private Skin skin;
 
     @Override
     public void create() {
         assetManager = new AssetManager();
         assetManager.load("uiskin.json", Skin.class);
-        // talvez seja necessário chamar uma tela de "Loading" caso os assets sejam muito grandes
-        // por enquanto dá pra deixar assim
         assetManager.finishLoading();
         skin = assetManager.get("uiskin.json", Skin.class);
 
         AnimationManager.load(); // carrega todas as animações dos sprites logo ao iniciar
+
+        Pixmap pixmap = new Pixmap(Gdx.files.internal("ponteiro.png"));
+        int xHotspot = 0;
+        int yHotspot = 0;
+        Cursor cursorPersonalizado = Gdx.graphics.newCursor(pixmap, xHotspot, yHotspot);
+        Gdx.graphics.setCursor(cursorPersonalizado);
 
         this.setScreen(new MainMenuScreen(this, skin));
     }
